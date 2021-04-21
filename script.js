b22854f8bc30fc290fcb11546ddd1f12bc128f7a
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-21 08:59:04 -0700

    Change to use nondeprecated version

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const snap = document.querySelector('.snap');
 function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
-    .then((localMediaStream) => {
-      console.log(localMediaStream);
-      video.srcObject = localMediaStream;
+    .then((MediaStream) => {
+      console.log(MediaStream);
+      video.srcObject = MediaStream;
       video.play();
     })
     .catch((err) => {
