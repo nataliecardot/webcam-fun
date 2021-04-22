9b0a0e2610c29ce1e05592ac2a6089983fa4c63e
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-22 14:21:18 -0700

    Change effect

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function paintToCanvas() {
     // pixels = redEffect(pixels);
 
     pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.7;
+    ctx.globalAlpha = 0.1;
 
     // pixels = greenScreen(pixels);
     // put them back
